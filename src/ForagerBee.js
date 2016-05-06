ae4eb388834597be1a4ed8f2846bc3876a73c288
dfuruya <dfuruya@gmail.com>
2016-05-06 12:05:35 -0700

    Pass all tests for 6ees6ees6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,7 +1,18 @@
 import Bee from './Bee'
 
-class ForagerBee {
-  // TODO..
-};
+class ForagerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.color = 'yellow';
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage() {
+    this.treasureChest.push('treasure');
+  }
+}
 
 export default ForagerBee;
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,7 +1,22 @@
 import ForagerBee from './ForagerBee';
 
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(props) {
+    super();
+    this.age = 40;
+    this.job = 'gamble';
+    this.color = 'grey';
+    this.canFly = false;
+    this.treasureChest = [];
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble() {
+    this.treasureChest.push('treasure');
+  }
 };
 
 export default RetiredForagerBee;
